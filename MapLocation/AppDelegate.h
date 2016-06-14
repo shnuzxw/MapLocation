3924f5f4b1fb89de949a997d70e322fd3f9d311e
张校玮 <weixiao08@qq.com>
2016-06-14 09:21:00 +0800

    Initial Commit

---
+++ MapLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapLocation
+//
+//  Created by smile.zhang on 16/6/14.
+//  Copyright © 2016年 smile.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
